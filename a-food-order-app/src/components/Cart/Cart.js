baf9bf8313f01108755dd6dbfc14e163b3d0beaa
ishmam00 <93041311+ishmam00@users.noreply.github.com>
2023-03-24 00:21:29 +0600

    Added a Modal via a React Portal

--- a-food-order-app/src/components/Cart/Cart.js
+++ a-food-order-app/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -10,7 +11,7 @@ const Cart = (props) => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -18,8 +19,10 @@ const Cart = (props) => {
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
-        <button></button>
+        <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
+
+export default Cart;
